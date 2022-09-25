b77943dbf2d3c302ca423c55531ddda503d620f6
Michael Ye <michye@Michaels-MacBook-Air.local>
2022-09-24 23:54:11 -0400

    added mainpage functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+import { Home } from './components/Home'
+import { NavigationBar } from './components/NavigationBar'
+// import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
+          <Routes>
+            <Route path="/" element={<><Home/><NavigationBar/></>} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
