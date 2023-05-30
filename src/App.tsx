f9cd4441706829f422a2d026b5f3426ffdc6f78b
yemichael123 <michye@umich.edu>
2023-05-30 00:38:35 -0400

    added fomantic library, redesigned header

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import logo from './logo.svg';
 import { Home } from './components/Home'
 import { NavigationBar } from './components/NavigationBar'
 import { Games } from './components/Games'
+import './semantic/dist/semantic.css';
+import { Loader } from 'semantic-ui-react';
+
 // import './App.scss';
 
 function App() {
   return (
     <div className="App">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/dist/semantic.min.css"></link>
+        <script src="/dist/semantic.min.js"></script>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
-            <Route path="/" element={<><Games/><NavigationBar/></>}>
-            </Route>
-            <Route path="games" element={<Games/>} />
+            <Route path="/" element={<><Home/><NavigationBar/></>} />
+            <Route path="games" element={<><Games/><NavigationBar/></>} />
           </Routes>
       </BrowserRouter>
     </div>
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,20 +1,25 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Button } from 'semantic-ui-react';
 
 export const NavigationBar = () => {
     return (
-      <Navbar className="px-3 fixed-top navbar-transparent" expand={true}>
-      <Container fluid>
+      <Navbar className="px-3 fixed-top navbar-transparent" expand="lg">
+        <Container fluid>
           <Nav></Nav>
           <Nav
             className="ml-auto"
             style={{ alignItems: 'Right', }}
           >
-            <Nav.Link className="navigation" href="/">Home</Nav.Link>
+            <Button.Group>
+              <Button inverted size="massive" onClick={() => {window.location.href = "/";}}>Home</Button>
+              <Button inverted size="massive" onClick={() => {window.location.href = "/";}}>Resume</Button>
+              <Button inverted size="massive" onClick={() => {window.location.href = "/";}}>Projects</Button>
+            </Button.Group>
+            {/* <Nav.Link className="navigation" href="/">Home</Nav.Link>
             <Nav.Link className="navigation">Resume</Nav.Link>
             <NavDropdown align="end" className="navigation" title="Hobbies" id="dropdown-menu dropdown-menu-left">
               <NavDropdown.Item href="#action3">Origami</NavDropdown.Item>
@@ -24,7 +29,7 @@ export const NavigationBar = () => {
               <NavDropdown.Item href="#action5">
                 Something else here
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
       </Container>
     </Navbar>
